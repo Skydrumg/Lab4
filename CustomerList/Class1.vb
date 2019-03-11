d42e8a246953f6a51306284c2a94c33846171bf3
Skydrumg <cpmartin.on.ca@gmail.com>
2019-03-11 12:57:38 -0400

    welp

---
+++ CustomerList/Class1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Car
+
+    Private carModel As String = String.Empty
+    Private carMake As String = String.Empty
+    Private carYear As Integer
+    Private carPrice As Integer
+    Private carIfUsed As Boolean = False
+
+
+End Class
